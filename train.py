c4ca31e4678e9d9e6efebdfb0135d5f0415c7442
Anton Morozov <antonmorozoff64@gmail.com>
2022-04-09 21:56:21 +0600

    training with spectrograms

--- train.py
+++ train.py
@@ -1,11 +1,26 @@
 from tensorflow.keras.datasets import mnist
 import numpy as np
+import os
 
-from autoencoder import Autoencoder
+from vae import VAE
 
 LEARNING_RATE = 0.0005
-BATCH_SIZE = 32 
-EPOCHS = 20
+BATCH_SIZE = 64
+EPOCHS = 150
+
+SPECTROGRAMS_PATH = './free-spoken-digit-dataset/spectrogram'
+
+def load_fsdd(spectrograms_path):
+  x_train = []
+  for root, _, file_names in os.walk(spectrograms_path):
+    for file_name in file_names:
+      file_path = root + '/' + file_name
+      spectrogram = np.load(file_path)
+      x_train.append(spectrogram)
+  x_train = np.array(x_train)
+  x_train = x_train[..., np.newaxis]
+  print(x_train.shape)
+  return x_train  
 
 def load_mnist():
   (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -19,7 +34,8 @@ def load_mnist():
   return x_train, y_train, x_test, y_test
 
 def train(x_train, learning_rate, batch_size, epochs):
-  autoencoder = Autoencoder(input_shape=(28,28,1), conv_filters=(32, 64, 64, 64), conv_kernels=(3, 3, 3, 3), conv_strides=(1, 2, 2, 1), latent_space_dim=2)
+  autoencoder = VAE(input_shape=(256,64,1), conv_filters=(512, 256, 128, 64, 32), conv_kernels=(3, 3, 3, 3, 3), conv_strides=(2, 2, 2, 2, (2,1)), latent_space_dim=128)
+  # autoencoder = Autoencoder(input_shape=(28,28,1), conv_filters=(32, 64, 64, 64), conv_kernels=(3, 3, 3, 3), conv_strides=(1, 2, 2, 1), latent_space_dim=2)
   autoencoder.summary()
   autoencoder.compile(learning_rate)
   autoencoder.train(x_train, batch_size, epochs)
@@ -27,6 +43,9 @@ def train(x_train, learning_rate, batch_size, epochs):
   
 
 if __name__ == "__main__":
-  x_train, _, _, _ = load_mnist()
-  autoencoder = train(x_train[:10000], LEARNING_RATE, BATCH_SIZE, EPOCHS)
+  # x_train, _, _, _ = load_mnist()
+  # autoencoder = train(x_train[:10000], LEARNING_RATE, BATCH_SIZE, EPOCHS)
+  # autoencoder.save("model")
+  x_train = load_fsdd(SPECTROGRAMS_PATH)
+  autoencoder = train(x_train, LEARNING_RATE, BATCH_SIZE, EPOCHS)
   autoencoder.save("model")
